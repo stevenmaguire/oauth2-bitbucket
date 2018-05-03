6c5037f4728745b002108f4018e66f826f610614
Getulio Sánchez <valentin2507@gmail.com>
2018-05-02 20:20:19 -0400

    Use comma as separator in test

--- test/src/Provider/BitbucketTest.php
+++ test/src/Provider/BitbucketTest.php
@@ -42,7 +42,7 @@ class BitbucketTest extends \PHPUnit_Framework_TestCase
 
     public function testScopes()
     {
-        $scopeSeparator = ',';
+        $scopeSeparator = ' ';
         $options = ['scope' => [uniqid(), uniqid()]];
         $query = ['scope' => implode($scopeSeparator, $options['scope'])];
         $url = $this->provider->getAuthorizationUrl($options);
